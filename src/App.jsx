8cff10aaabad3663c1829b462fe8b9dbb83853e3
Halcyon <hashem_elmaghrabi@yahoo.com>
2024-10-17 17:35:38 -0400

    Imported App.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Outlet } from 'react-router-dom';
 import Header from './pages/Header';
 import Footer from './pages/Footer';
+import './App.css'
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
